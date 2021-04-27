b2b6ef41a0775af9e12e6051bbb41a6993806805
HaomeiLiu <haomeili@usc.edu>
2021-04-27 22:39:34 +0800

    Initial Commit

---
+++ practice40h/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice40h
+//
+//  Created by 刘昊嵋 on 2021/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
