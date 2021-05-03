753c2153fa9ce8a73c6b908317a5cc28f59dc434
HaomeiLiu <haomeili@usc.edu>
2021-05-03 15:29:29 +0800

    Calendar functions tested

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'practice40h' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for practice40h
+  # pod 'Firebase/Auth'
+  # pod 'Firebase/Firestore'
+  pod 'FSCalendar'
+  pod 'KDCircularProgress'
+
+  target 'practice40hTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'practice40hUITests' do
+    # Pods for testing
+  end
+
+end
--- practice40h/Controller/ViewController.swift
+++ practice40h/Controller/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
+    var calender: FSCalendar!
+    var formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calender = FSCalendar(frame: CGRect(x: 0.0, y: 40.0, width: self.view.frame.size.width, height: 300.0))
+        //calender.scope = .week
+        //calender.locale = Locale(identifier: "ch")
+        calender.scrollDirection = .vertical
+        self.view.addSubview(calender)
+        
+        calender.delegate = self
+        calender.dataSource = self
+        calender.allowsMultipleSelection = true
     }
+    
+    //Delegate
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        formatter.dateFormat = "dd-MMM-yyyy"
+        print("\(formatter.string(from: date))")
+    }
+    
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        //
+    }
+    
+    func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
+        formatter.dateFormat = "dd-MM-yyyy"
+        guard let excludedDate = formatter.date(from: "23-07-2020")
+        else{
+            return true
+        }
+        if date.compare(excludedDate) == .orderedSame{
+            return false
+        }
+        return true
+    }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        return 0
+    }
+    
+    //Delegate Appearance
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        //Same as shouldSelect
+        return nil
+    }
+    
+    //Data Source
+    func minimumDate(for calendar: FSCalendar) -> Date {
+        return Date()
+    }
+    
+    func maximumDate(for calendar: FSCalendar) -> Date {
+        return Date().addingTimeInterval((24*60*60)*5)
+    }
+    
 
 
 }
+
 
